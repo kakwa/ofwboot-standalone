75c1829fcf8f17ccda6c1f9a0b0f0b9125040f21
kakwa <carpentier.pf@gmail.com>
2025-09-15 13:39:16 +0200

    adding OpenBSD base code

---
+++ libsa.d
@@ -0,0 +1,13 @@
+libsa.o: \
+ /home/carpenti/Geek/Personal/ofwboot-standalone/deps/lib/libkern/qdivrem.c \
+ /home/carpenti/Geek/Personal/ofwboot-standalone/deps/lib/libkern/quad.h \
+ /home/carpenti/Geek/Personal/ofwboot-standalone/sys/types.h \
+ /home/carpenti/Geek/Personal/ofwboot-standalone/sys/cdefs.h \
+ /home/carpenti/Geek/Personal/ofwboot-standalone/machine/cdefs.h \
+ /home/carpenti/Geek/Personal/ofwboot-standalone/sys/endian.h \
+ /home/carpenti/Geek/Personal/ofwboot-standalone/sys/_endian.h \
+ /home/carpenti/Geek/Personal/ofwboot-standalone/sys/_types.h \
+ /home/carpenti/Geek/Personal/ofwboot-standalone/machine/_types.h \
+ /home/carpenti/Geek/Personal/ofwboot-standalone/machine/endian.h \
+ /home/carpenti/Geek/Personal/ofwboot-standalone/sys/limits.h \
+ /home/carpenti/Geek/Personal/ofwboot-standalone/machine/limits.h
